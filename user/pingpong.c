116c3459b0f8e09c416e3de23df1cf4dc198b294
200110515 <200110515@comp5.hwccluster.labcenter>
2022-09-19 14:47:04 +0000

    实验1完成前三题

---
+++ user/pingpong.c
@@ -0,0 +1,37 @@
+#include "kernel/types.h"
+#include "user.h"
+
+int main(){
+    int parent_fd[2], sub_fd[2];
+    char buffer[12];        // 创建12个字符的空间作为缓冲区
+    pipe(parent_fd);
+    pipe(sub_fd);
+    int fork_ret = fork();
+    if (fork_ret == 0)      // 子进程
+    {
+        // 从管道读取父进程信息
+        close(parent_fd[1]);
+        read(parent_fd[0], buffer, 4);
+        // 输出
+        buffer[4] = '\0';
+        printf("%d: received %s\n", getpid(), buffer);
+        // 写入管道
+        close(sub_fd[0]);
+        write(sub_fd[1], "pong", 4);
+        exit(0);
+    }
+    else if (fork_ret > 0)  // 父进程
+    {
+        // 写入管道
+        close(parent_fd[0]);
+        write(parent_fd[1], "ping", 4);
+        // 从管道读取子进程信息
+        close(sub_fd[0]);
+        read(sub_fd[0], buffer, 4);
+        // 输出
+        buffer[4] = '\0';
+        printf("%d: received %s\n", getpid(), buffer);
+        wait(0);
+    }
+    exit(0);
+}
---
+++ user/primes.c
@@ -0,0 +1,45 @@
+#include "kernel/types.h"
+#include "user.h"
+
+void sub_fork(int p_read){
+    int prime;
+    if (read(p_read, &prime, 4) !=0){
+        int current_fd[2];
+        pipe(current_fd);
+        printf("prime %d\n", prime);
+        if (fork()==0){
+            close(current_fd[1]);
+            sub_fork(current_fd[0]);
+        }
+        else
+        {
+            int temp;
+            while (read(p_read, &temp, 4) != 0){
+                if(temp % prime != 0){
+                    write(current_fd[1], &temp, 4);
+                }
+            }
+            close(current_fd[1]);
+            wait(0);
+        }
+    }
+    exit(0);
+}
+
+int main()
+{
+    int p[2];
+    pipe(p);
+    if(fork() == 0){
+        close(p[1]);
+        sub_fork(p[0]);
+    }else{
+        for (int i = 2; i < 36; i++)
+        {
+            write(p[1], &i, 4);
+        }
+        close(p[1]);
+        wait(0);
+    }
+    exit(0);
+}
